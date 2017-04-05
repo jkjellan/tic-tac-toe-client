46f336011c9b635436f2552d7c9d3bd22f392afa
jkjellan <jkjellan@gmail.com>
2017-04-05 10:38:21 -0400

    Adds game logic to tic-tac-toe game
    I created a gameLogic.js file, exporting three functions that
    handle 1) placing Xs and Os when player clicks on board,
    2) checking win conditions and ending play if met and 3) rendering
    the game board.  One bug that remains is the draw counter, which
    increases by 2 (instead of 1) every time there is a draw, and continues
    to increase with clicks even after the game ends.  There is also a
    console error, which doesn't break the game, but needs to be resolved.
    The console error has to do with the gameBoard array being undefined
    before play starts

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,28 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const gameLogic = require('./gameLogic')
+
+// global variables.  Do these belong somewhere else?
+const boardArray = []
+const xLeft = ['x', 'x', 'x', 'x', 'x']
+const oLeft = ['o', 'o', 'o', 'o']
+const scoreArray = [0, 0, 0]
+const playerArray = ['JIM', 'DRAW', 'MELISSA']
+let xWin = false
+let oWin = false
+let winner = ''
+
+$('.p1-prompt-text').html("It's your turn, " + playerArray[0] + '!')
+$('.p2-prompt-text').html('')
 
 $(() => {
   setAPIOrigin(location, config)
+  console.log('this is my tic tac toe game')
+  gameLogic.handleClick()
+  gameLogic.checkWin()
+  gameLogic.renderBoard()
+
 })
 
 // use require with a reference to bundle the file and use it in this file
